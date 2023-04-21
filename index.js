3f8fc30155b3674c67bd930514d97d56b025f1c2
nicopina <pinanicolasagustin@gmail.com>
2023-04-21 18:31:02 -0300

    metodos mutables

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-let numbers = [2,5,6,3,10,6,7]
+let beers = ['Andes', 'Corona', 'Patagonia', 'Salta']
 
-show(numbers)
+//metodo MUTABLE - modifican el estado
+beers.push('Heineken')
+beers.unshift('Grolsh')
+beers.splice(2, 0, "Antares") // el segundo parametro sirve para eliminar si se selecciona de 1 para arriba
+
+let b = beers.pop() //regresa el ultimo elemento pero lo elimina
+let b2 = beers.shift() //regresa el primer elemento pero lo elimina
+
+//metodo INMUTABLE
 
 
 
+
+showDom('array1', numbers)
 
 function show(arr){
-    for(let i=0; i<numbers.length; i++){
-        console.log(numbers[i]);
+    for(let i=0; i<arr.length; i++){
+        console.log(arr[i]);
     }
 }
+ function showDom(element){
+    document.getElementById(element).innerHTML = "";
+
+    for(let i=0; i<arr.length; i++){
+        document.getElementById(element).innerHTML += 
+        `<div> ${arr[i]} </div>`
+    }
+ }
