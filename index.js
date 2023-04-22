fb8eda4436decdc92ad5d8a1968075617f7a722e
nicopina <pinanicolasagustin@gmail.com>
2023-04-22 00:17:59 -0300

    funciones de orden superior tomando el valor de las de 1er orden

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-let beers = ['Andes', 'Corona', 'Patagonia', 'Salta']
+const beers = ['Andes', 'Corona', 'Patagonia', 'Salta']
+
+
 let wines = ['Luigi Bosca', 'Nieto Senetiner', 'Trumpeter', 'Benjamin']
 
 //metodo MUTABLE - modifican el estado
 beers.push('Heineken')
 beers.unshift('Grolsh')
 beers.splice(2, 0, "Antares") // el segundo parametro sirve para eliminar si se selecciona de 1 para arriba
+beers.fill('Artesanal', 0, beers.length-1)
 
-let b = beers.pop() //regresa el ultimo elemento pero lo elimina
+
+
+
+/* let b = beers.pop() //regresa el ultimo elemento pero lo elimina
 let b2 = beers.shift() //regresa el primer elemento pero lo elimina
+ */
+
 
 //metodo INMUTABLE
 const alcoholDrinks = beers.concat(wines)
 alcoholDrinks.push('Callia')
+const beers2 = beers.slice(0, 2)
 
 showDom('array1', beers)
 showDom('array2', alcoholDrinks)
 
 
+//HELPERS --------------------------------
 function show(arr){
     for(let i=0; i<arr.length; i++){
         console.log(arr[i]);
--- practicas/practica2.js
+++ practicas/practica2.js
@@ -13,8 +13,62 @@ function multiplicar(a, b, c, d, e){
     return a*b*c*d*e
 } */
 
-let person = {name: 'Nicolao', age: 27}
+/* let person = {name: 'Nicolao', age: 27}
 let person2 = { ...person}
 person2.age = 28
 console.log(person)
-console.log(person2)
+console.log(person2) */
+
+const beers = [{
+    name: 'heineken',
+    price: 4,
+    alcohol: 4.5
+    },
+    {
+    name: 'antares',
+    price: 5,
+    alcohol: 4
+    },
+    {
+    name: 'corona',
+    price: 3,
+    alcohol: 3
+    },
+    {
+    name: 'grolsh',
+    price: 4.5,
+    alcohol: 5
+    }
+]
+
+let fn = function(){//Funcion de primer orden
+    console.log('Hellooo')
+}
+                                //Dentro de la funcion de ord sup le pasamos como valor la funcion de 1er orden
+                                //se le asigna a some(f) y luego f es llamada dentro de la function
+function some(f){//Funcion de orden superior
+    console.log('algo antes')
+    f()
+    console.log('algo despues')
+}
+some(fn)                        //al ser fn(la fun de 1er orden) el valor de some(), 'f' tomará el valor de fn 
+
+showDom('array1', beers)
+
+function show(arr){
+    for(let i=0; i<arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+ function showDom(element){
+    document.getElementById(element).innerHTML = "";
+
+    for(let i=0; i<arr.length; i++){
+        document.getElementById(element).innerHTML += 
+        `<div> 
+            ${arr[i].name} 
+            $${arr[i].price}
+            (${arr[i].alcohol})    
+        </div>`
+    }
+ }
