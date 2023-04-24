797928f7d976cffcc0dabd1086d388e6db7b2f54
nicopina <pinanicolasagustin@gmail.com>
2023-04-24 20:43:21 -0300

    practica fetch con reduce

--- practicas2/01.js
+++ practicas2/01.js
@@ -10,11 +10,26 @@ function print(content){
     myDiv.innerHTML = htmlContent
 }
 
+function printClosure(htmlElement, fields){ //utilizamos field para darle responsabilidad unica en vez de
+                                        //acoplarlo a un campo en particular
+    return (content)=>{ //se retorna la funcion lista para ejecutarse, asi nos da la posiblidad para mandarla
+        let htmlContent = ''
+
+        content.forEach((element) => {
+            let text = fields.reduce((acc, e)=>
+                acc + element[e] + ' ', '')
+            htmlContent += `<div>${text}</div>`
+            })
+            htmlElement.innerHTML = htmlContent    //como parametro para ser usada por una fn de 1er orden
+    }
+}
+
 async function request(url, fn){
     let response = await fetch(url)
     let json = await response.json()
-    fn(json)
+    fn(json)                            //cuando se ejecuta aqui entra como callback
 }
 let url = 'https://jsonplaceholder.typicode.com/todos/'
 request(url, log)
-request(url, print)
+//request(url, print)
+request(url, printClosure(myDiv, ['userId','title', 'id']))
