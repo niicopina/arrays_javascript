1fa187e3ef631914abeac34a2773d5a3b3af4a25
nicopina <pinanicolasagustin@gmail.com>
2023-04-24 20:10:35 -0300

    server express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.listen(8000, ()=>{
+    console.log('ss', 8000)
+})
