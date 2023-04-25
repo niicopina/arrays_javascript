0abac2ae7c8929e61e6f73b92c71269981bc7789
nicopina <pinanicolasagustin@gmail.com>
2023-04-24 21:33:35 -0300

    ciclo de customer

--- practicas/practica6.js
+++ practicas/practica6.js
@@ -9,7 +9,7 @@ class CustomerService {
     work(task, charlar){
         this.tasks.push(task)
         this.timer += task.quantity
-        console.log(this.name + ' ha realizado la tarea '+task.name+', le llevó'+task.quantity+' minutos')
+        console.log(this.name + ' ha realizado la tarea '+task.name+', le llevó '+task.quantity+' minutos')
         if(this.timer >= this.limit){
             this.descansar()
         }
@@ -17,7 +17,33 @@ class CustomerService {
             charlar()
         }
     }
+    descansar(){
+        console.log(this.name+' ha ido de break')
+        this.timer = 0
+
+        this.callback()
+        console.log('... vuelve a realizar los ciclos')
+    }
 }
+
+task1 = {name: 'Cafe del día', quantity: 30}
+task2 = {name: 'Jarabes', quantity: 35}
+task3 = {name: 'Hielo', quantity: 40}
+task4 = {name: 'Tapas y Vasos', quantity: 25}
+task5 = {name: 'Condiment y mesas', quantity: 60}
+task6 = {name: 'Bolsas', quantity: 45}
+task7 = {name: 'Cremas', quantity: 35}
+task8 = {name: 'Jugos', quantity: 50}
+
+let cs = new CustomerService('Fran', 180, function(){console.log('se toma un aguita')})
+cs.work(task1)
+cs.work(task2)
+cs.work(task3)
+cs.work(task4)
+cs.work(task5)
+cs.work(task6, function(){console.log('va al baño')})
+cs.work(task7)
+cs.work(task8)
 
 let nico = {
     name: 'nico',
@@ -26,17 +52,30 @@ let nico = {
         console.log('se toma un cafecito')
     },
     tasks: [],
-    timer: 0
-
+    timer: 0,
+    work: function(task, charlar){
+        this.tasks.push(task)
+        this.timer += task.quantity
+        console.log(this.name+' ha realizado el ciclo'+task.name+', ha tardado '+task.quantity+' minutos')
+        if(this.timer >= this.limit){
+            this.descansar()
+        }
+        if(charlar){
+            charlar()
+        }
+    },
+    descansar(){
+        console.log(this.name+' ha ido de break')
+        this.timer = 0
+        this.callback()
+        console.log('vuelve a laburar')
+    }
 }
-
-let cs = new CustomerService('Fran', 180)
-
-task1 = {name: 'Cafe del día', quantity: 10}
-task2 = {name: 'Jarabes', quantity: 15}
-task3 = {name: 'Hielo', quantity: 20}
-task4 = {name: 'Tapas y Vasos', quantity: 25}
-task5 = {name: 'Condiment y mesas', quantity: 30}
-task6 = {name: 'Bolsas', quantity: 35}
-task7 = {name: 'Cremas', quantity: 35}
-task8 = {name: 'Jugos', quantity: 20}
+nico.work(task2)
+nico.work(task4)
+nico.work(task6)
+nico.work(task7)
+nico.work(task8)
+nico.work(task1)
+nico.work(task3)
+nico.work(task5)
