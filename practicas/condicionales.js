86c8e954cc87b6c93fa7cf0b7334b4ea06c90812
nicopina <pinanicolasagustin@gmail.com>
2023-05-09 23:09:47 -0300

    condicionales

---
+++ practicas/condicionales.js
@@ -0,0 +1,14 @@
+const age = 21
+let isAdult = false
+
+if(age>= 18){
+    console.log('mayor de edad')
+    isAdult = true
+}else{
+    console.log('menor de edad')
+    isAdult = false
+}
+if(isAdult){
+    alert('es adulto')
+}
+//switch case
