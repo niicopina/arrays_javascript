729e2442915b1b3e6f84c46b0a5efd9f32d08941
nicopina <pinanicolasagustin@gmail.com>
2023-06-07 21:22:33 -0300

    objects literals

--- practicas2/arrays/json/promesas.js
+++ practicas2/arrays/json/promesas.js
@@ -1,12 +1,12 @@
 //setTimeout(()=>console.log('hola un seg despues'), 1500)
 
-const myPromise = new Promise((res, rej)=> {
+/* const myPromise = new Promise((res, rej)=> {
     setTimeout(()=>{
         console.log('se ejecuta la promesa')
         rej('error algo paso')
         res('Pato')
     }, 1500)
-})
+}) */
 
 /* myPromise
     .then(data => console.log('El dato retornado por la promesa es: ' + data))
@@ -20,6 +20,41 @@ const myPromise = new Promise((res, rej)=> {
 /* (function(){
     console.log('es ima iofe')
 })(); */
-(async function(){
+/* (async function(){
     const dataResult = await fetch("https://jsonplaceholder.typicode.com/users")
-})();
+    const jsonResult = await dataResult.json();
+})(); */
+
+/* fetch('people.json')
+    .then(data=data.json())
+    .then(json => {
+        const people = json
+
+        console.log(people.name)
+    }) */
+
+const sale = {
+    customer: 'Hector',
+    total: 11000,
+    items: [{
+        name: '20pg monitor',
+        brand: 'LG'
+    },
+    {
+        name: 'keyboard',
+        brand: 'Soul'
+    },
+    {
+        name: 'Mouse',
+        brand: 'Logitech'
+    }],
+    showItems(){
+        this.items.forEach(i => console.log(i.name))
+    },
+    showInfo(){
+        console.log('Sale data is: ')
+        console.log(`Client: ${this.customer}, total: ${this.total}`)
+        this.showItems()
+    }
+}
+sale.showInfo()
