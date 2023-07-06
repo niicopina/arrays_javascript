7b68ecccde9b7c90f37f3c1d96a8fa273a206901
nicopina <pinanicolasagustin@gmail.com>
2023-07-06 15:46:47 -0300

    linked list

---
+++ practicas2/arrays/linkedList/linklist.js
@@ -0,0 +1,57 @@
+//conjunto de nodos enlazados, el cual genera un seguimiento
+//finaliza con NULL -> posicion de memoria que no tiene nada guardado pero esta reservado
+
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null
+    }
+    add(value){
+        const node = new Node(value)
+        if(this.head === null){
+            this.head = node
+        }else{
+            let current = this.head
+            while(current.next !== null){
+                current = current.next
+            }
+            current.next = node
+        }
+    }
+    delete(value){
+        
+    }
+    show(){
+        let current = this.head
+        while(current !== null){
+            console.log(current.value)
+            current = current.next
+        }
+    }
+    size(){
+        let count = 0
+        let current = this.head
+        while(current !== null){
+            count++
+            current = current.next
+        }
+        return count
+    }
+    clear(){
+        this.head = null
+    }
+}
+
+const linkedList = new LinkedList()
+linkedList.add('A')
+linkedList.add('B')
+linkedList.add('C')
+linkedList.add('D')
+console.log(linkedList.size())
+linkedList.show()
